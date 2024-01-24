e9b5426442b96e28261554b2fb0e4931be50b0fc
Kregiss <145288385+Kregiss@users.noreply.github.com>
2024-01-24 22:00:09 +0300

    Update bubblesort.c

--- Algorithms and data structures/bubblesort.c
+++ Algorithms and data structures/bubblesort.c
@@ -1,22 +1,35 @@
-#include <stdio.h>
-
 void bubblesort(unsigned long nel,
         int (*compare)(unsigned long i, unsigned long j),
-        void (*swap)(unsigned long i, unsigned long j))
+        void (*swap)(unsigned long i, unsigned long j)) 
 {
-    if (nel != 0){
-        unsigned long right_border = nel - 1, left_border = 0, last_right_border, last_left_border;
-        int fl = 1;
-        for (unsigned long i = 0; i < right_border; i++) {
-            for (unsigned long j = 0; j < right_border - i; j++) {
+    unsigned long left_border = 0, right_border = nel - 1;
+    int swapped;
 
-                if (fl == -1) {
-                    if (compare(j, j + 1) == 1) { swap(j, j + 1); }
-                } else {
-                    if (compare(j + 1, j) == -1) { swap(j + 1, j); }
+    if (nel != 0)
+    {
+        do {
+            // Проход слева направо
+            swapped = 0;
+            for (unsigned long i = left_border; i < right_border; i++) {
+                if (compare(i + 1, i) < 0) {
+                    swap(i + 1, i);
+                    swapped = 1;
                 }
             }
-            fl *= -1;
-        }
+            if (!swapped) break;
+
+            right_border--;
+
+            // Проход справа налево
+            swapped = 0;
+            for (unsigned long i = right_border; i > left_border; i--) {
+                if (compare(i, i - 1) < 0) {
+                    swap(i, i - 1);
+                    swapped = 1;
+                }
+            }
+            
+            left_border++;
+        } while (swapped);
     }
 }
